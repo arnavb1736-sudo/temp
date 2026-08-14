0a1b2c1e9a4051ab83d0a8a9f1d3a0eb1b14065b
Elevastra <gaurav.himkar@elevastraglobal.com>
2026-08-14 21:18:09 +0530

    Update LinkedIn API version in test file

--- app/linkedin_post_test.py
+++ app/linkedin_post_test.py
@@ -8,7 +8,7 @@ url = "https://api.linkedin.com/rest/posts"
 
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
-    "LinkedIn-Version": "202507",
+    "LinkedIn-Version": "202607",
     "X-Restli-Protocol-Version": "2.0.0",
     "Content-Type": "application/json",
 }
